3b0e8897c2ba8faa3b49ae30d0be1f7de57c83c5
sorenvindmaersk <soren.vind@maersk.com>
2019-11-17 07:48:25 +0100

    New wrapper script takes place of old install.sh

--- install.sh
+++ install.sh
@@ -1,88 +1,28 @@
 #!/bin/bash
 
-# Inspiration: http://fredkelly.net/articles/2014/10/19/developing_on_yosemite.html
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+SETUP_MODULES_PATH="$DIR/modules"
 
-# Set up OSX settings
-./OSXsettings.sh
+import_module() {
+    module_name=$1
+    echo "Importing module $module_name"    
+    source "$SETUP_MODULES_PATH/$module_name"
+}
 
-# Install homebrew and cask
-if test ! $(which brew)
-then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew doctor
-  brew tap caskroom/cask
-fi
+echo "Installation starting."
+for mod in `ls $SETUP_MODULES_PATH`; do
+    import_module $mod
+done
 
-# Install fonts
-unzip -q Input-Font.zip -d inputfont
-cp inputfont/Input_Fonts/*/*/*.ttf /Library/Fonts/
-rm -fr inputfont
-brew tap caskroom/fonts
-brew cask install font-hack
-brew cask install font-source-code-pro
-
-# Install own utilities
-mkdir ~/bin
-cp -f git-subdir-status.sh ~/bin/
-chmod +x ~/bin/*
-
-# Install latest zsh and oh-my-zsh
-OMZ=~/.oh-my-zsh
-if test ! -d $OMZ
-then
-  brew install zsh
-  curl -L http://install.ohmyz.sh | sh
-fi
-
-### TODO: PIP FIX (path)
-
-# Install common sanity
-brew update
-brew upgrade
-brew install vim wget curl tree go node pstree ruby make git zsh-syntax-highlighting
-brew install awscli
-brew install git-crypt telnet mssql-tools
-
-# k8s
-brew install kubernetes-cli kubectx
-
-# Install python packages
-pip install pygments requests virtualenv pipenv
-
-# Install dot configs
-cp -f soren.vimrc ~/.vimrc
-cp -f soren.zshrc ~/.zshrc
-mkdir -p ~/.ssh
-cp -f soren.ssh.config ~/.ssh/config
-cp -f soren.gitconfig ~/.gitconfig
-cp -f soren.gitignore_global ~/.gitignore_global
-
-# Install Real Apps [tm]!
-brew cask install google-chrome
-brew cask install vagrant
-brew cask install gitup
-brew cask install moom
-brew cask install slack
-brew cask install visual-studio-code
-brew install httpie yamllint
-
-# Bleeding edge docker
-brew tap homebrew/cask-versions
-brew cask install docker-edge
-brew install kubectx
-
-# Corporate tooling
-brew cask install microsoft-office jetbrains-toolbox
-brew cask install java keybase
-
-
-
-## TODO: Moom Configuration, Intellij Configuration, Terminal Configuration
-## TODO: SSH KEYS -> https://help.github.com/articles/generating-an-ssh-key/
-
-
-#echo "Install solarized terminal template to finish installation:"
-#echo "  Use solarized-dark-sovi.terminal as your terminal default colors."
+config_macos_settings
+install_brew
+install_shell
+install_fonts
+install_utilities
+install_base
+install_apps
+install_configs
 
 echo "Setup terminal and xcode to use Pro template, with Input Mono Extra Light 12pt, antialias non-bold text and block cursor."
 echo "Also (IMPORTANT): uncheck 'set locale environment variables on startup' under advanced in the terminal settings."
